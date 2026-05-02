ecc4b939513cf17769a47f9f5335d0f87e510ded
Stas R <devtest.sr16@gmail.com>
2026-05-03 00:24:39 +0300

    First commit

--- demo2/src/main/java/Main_1.java
+++
@@ -1,6 +0,0 @@
-public class Main_1 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("World");
-    }
-}
--- untitled1/src/main/java/demo/main.java
+++
@@ -1,7 +0,0 @@
-package demo;
-
-public class main
-
-    }
-}
-// TODO сменить цвет
